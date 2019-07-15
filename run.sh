3ea2ab5db5e0ab6d7461dcaf5167e4f39b1defc2
sahitpj <jayakrishna.sahit@iitgn.ac.in>
2019-07-15 11:57:39 +0530

    added guide to run server locally

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "Make sure you have python3 installed"
+
+echo "Installing python dependencies"
+
+
+pip3 install -r requirements.txt
+
+
+echo "run stanford coreNLP at localhost:9000."
+echo "If not present download it and in the root folder run - java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000"
+
+echo "starting server in debug mode"
+python3 run_framework.py
