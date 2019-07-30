c7c56cd0a495da99165aedb8414c3b276d101954
sahitpj <jayakrishna.sahit@iitgn.ac.in>
2019-07-30 10:57:59 +0530

    production constants updated

--- app/src/pyspotlight/spotlight/config.py
+++ app/src/pyspotlight/spotlight/config.py
@@ -5,7 +5,7 @@ class Config(object):
         The following config class specifies the spotlight address in general. This service is the web version of it.
         '''
 
-        self.spotlight_address = "http://api.dbpedia-spotlight.org/en/annotate"
+        self.spotlight_address = "http://localhost:2222/rest/annotate"
 
         self.alternate_address = None
 
